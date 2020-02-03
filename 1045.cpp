52800587be8dbfa76e7437cc49ce116355178743
NN <NNzxcv@outlook.com>
2020-02-03 15:18:35 +0800

    UPDATE 1045.CPP

--- 1045.cpp
+++ 1045.cpp
@@ -2,15 +2,13 @@
 
 #include <iostream>
 #include <array>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-int find_max_index(int *lis, int n, int end)
+int find_max_index(int *lis, int end)
 {
-    if (end >= n)
-        exit(1);
     int max = 0;
-    for (int i = 0; i <= end; i++)
+    for (int i = 0; i < end; i++)
     {
         if (lis[i] > lis[max])
         {
@@ -19,37 +17,66 @@ int find_max_index(int *lis, int n, int end)
     }
     return max;
 }
+int *read_n(int n)
+{
+    int *arr = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    return arr;
+}
+int *get_hash(int *lst_like, int color_num, int lst_num)
+{
+    //lst_like   1-N
+    color_num++;
+    int *hash = (int *)malloc(sizeof(int) * color_num);
+    fill(hash, hash + color_num, -1);
+    for (int i = 0; i < lst_num; i++)
+    {
+        hash[lst_like[i]] = i;
+    }
+    return hash;
+}
+int convert_hash(int *lst, int *hash, int n)
+{
+    int lst_num = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (hash[lst[i]] != -1)
+        {
+            lst[lst_num] = hash[lst[i]];
+            lst_num++;
+        }
+    }
+    return lst_num;
+}
+
+int max_len(int *lst, int lst_num, int like_num)
+{
+    int *score = (int *)malloc(sizeof(int) * like_num);
+    fill(score, score + like_num, 0);
+    int num, max;
+    for (int i = 0; i < lst_num; i++)
+    {
+        num = lst[i];
+        max = find_max_index(score, num + 1);
+        score[num] = score[max] + 1;
+    }
+    int res = score[find_max_index(score, like_num)];
+    free(score);
+    return res;
+}
 
 int main(void)
 {
-    int n, m, k;
-    //
-    cin >> n;
-    int *hash = (int *)malloc(sizeof(int) * (n + 1));
-    fill(hash,hash+n+1,-1);
-    //
-    cin >> m;
-    for (int i = 0; i < m; i++)
-    {
-        int num;
-        cin >> num;
-        hash[num] = i;
-    }
-    //
-    cin >> k;
-    int *score = (int *)malloc(sizeof(int) * m);
-    fill(score,score+m,0);
-    for (int i = 0; i < k; i++)
-    {
-        int num;
-        cin >> num;
-        num = hash[num];
-        if (num != -1)
-        {
-            int max = find_max_index(score, m, num);
-            score[num] = score[max] + 1;
-        }
-    }
-    cout << score[find_max_index(score, m, m - 1)];
+    int color_num, like_num, lst_num;
+    cin >> color_num;
+    cin >> like_num;
+    int *like = read_n(like_num);
+    cin >> lst_num;
+    int *lst = read_n(lst_num);
+
+    int *hash = get_hash(like, color_num, like_num);
+    int lis_num_new = convert_hash(lst, hash, lst_num);
+    cout << max_len(lst, lis_num_new, like_num);
     return 0;
 }
