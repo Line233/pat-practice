10bf9c84d967bbfb7614e73781e0f0e377e01642
NNzxcv <NNzxcv@outlook.com>
2020-04-09 01:08:38 +0800

    update 1068-30-4

--- source/1068-30-4.cpp
+++ source/1068-30-4.cpp
@@ -1,14 +1,14 @@
 //https://pintia.cn/problem-sets/994805342720868352/problems/994805402305150976
 /*SUMMARY 
-    动态规划问题
+    动态规划问题,背包问题
     或者也可以深度/广度遍历（性能不好）
 */
-//TODO fix bug ,动态规划
 #include <iostream>
 #include <algorithm>
 #include <stdio.h>
 #include <vector>
 #include <stdio.h>
+#include <map>
 
 using namespace std;
 
@@ -21,100 +21,55 @@ typedef struct node
 } Node;
 // vector<int> Path;
 vector<int> Indexes;
+vector<int> MaxC;
+map<pair<int, int>, int> Path;
 int Sum = 0;
 int Money;
-bool dfs(int index)
-{
-    int last;
-    if (index == 0)
-        last = -1;
-    else
-        last = Indexes[index - 1];
-    for (int i = last + 1; i < Coins.size(); i++)
-    {
-        if (Sum + Coins[i] == Money)
-        {
-            Indexes.push_back(i);
-            return true;
-        }
-        else if (Sum + Coins[i] < Money)
-        {
-            Sum += Coins[i];
-            Indexes.push_back(i);
-            if (dfs(index + 1))
-                return true;
-            else
-            {
-                Sum -= Coins[i];
-                Indexes.pop_back();
-            }
-        }
-        else //>Money
-            return false;
-    }
-    return false;
-}
-bool bfs(int index)
-{
-    int last, maxindex;
-    if (index == 0)
-        last = -1;
-    else
-        last = Indexes[index - 1];
-    for (int i = last + 1; i < Coins.size(); i++)
-    {
-        if (Sum + Coins[i] > Money)
-        {
-            maxindex = i;
-            break;
-        }
-    }
-    for (int i = last + 1; i < maxindex; i++)
-    {
-        if (Sum + Coins[i] == Money)
-        {
-            Indexes.push_back(i);
-            return true;
-        }
-        if (Sum + Coins[i] < Money)
-        {
-            Indexes.push_back(i);
-            Sum += Coins[i];
-            if (bfs(index + 1))
-                return true;
-            else
-            {
-                Sum -= Coins[i];
-                Indexes.pop_back();
-            }
-        }
-        else if (Sum + Coins[i] > Money)
-        {
-            return false;
-        }
-    }
-    return false;
-}
 
 int main(void)
 {
     int n;
     cin >> n >> Money;
+    MaxC.resize(Money + 1);
+    fill(MaxC.begin(), MaxC.end(), 0);
     for (int i = 0; i < n; i++)
     {
         int k;
         scanf("%d", &k);
         Coins.push_back(k);
     }
-    sort(Coins.begin(), Coins.end());
-    if (bfs(0))
+    sort(Coins.rbegin(), Coins.rend()); //升序
+    /*
+    迭代，选前i个硬币时，最大容量为j时，能够到达的容量
+    */
+    for (int i = 0; i < Coins.size(); i++)
     {
-        for (int i = 0; i < Indexes.size(); i++)
+        for (int j = Money; j >= Coins[i]; j--)
         {
-            cout << Coins[Indexes[i]] << (i == Indexes.size() - 1 ? "" : " ");
+            if (MaxC[j] <= MaxC[j - Coins[i]] + Coins[i])
+            {
+
+                MaxC[j] = MaxC[j - Coins[i]] + Coins[i];
+                Path[{i, j}] = 1;
+            }
         }
     }
-    else
+
+    if (MaxC[Money] != Money)
         cout << "No Solution";
+    else
+    {
+        vector<int> p;
+        for (int i = Coins.size() - 1; i >= 0 && Money != 0; i--)
+        {
+            if (Path.count({i, Money}) != 0)
+            {
+                p.push_back(Coins[i]);
+                Money -= Coins[i];
+            }
+        }
+        for (auto ite = p.begin(); ite != p.end(); ite++)
+            cout << *ite << ((ite == p.end() - 1) ? "" : " ");
+    }
     return 0;
 }
